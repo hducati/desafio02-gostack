1fd98a7766bf56556085864c1ada401f221ae268
unknown <henrique.dcti@gmail.com>
2020-07-26 16:55:33 -0300

    All routes configured

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require('uuidv4');
 
 // const { uuid } = require("uuidv4");
 
@@ -9,22 +10,23 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+const likes = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  const {title, url, techs} = request.body
+  const {title, url, techs, likes} = request.body
 
-  const repositorie = {id: uuid(), title, url, techs}
+  const repositorie = {id: uuid(), title, url, techs, likes}
   repositories.push(repositorie)
 
   return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  const id = request.params
+  const { id } = request.params
   const {title, url, techs} = request.body
 
   const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
@@ -60,7 +62,18 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+
+    if (repositorieIndex < 0) {
+      response.status(400).json({error: 'Not found'})
+    }
+
+    const like = {id: id, likes: 1}
+    likes.push(like);
+
+    return response.json(like)
 });
 
 module.exports = app;
